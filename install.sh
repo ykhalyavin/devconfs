21f17e6f3ac0c3d67e479b604e790cc0ac98d776
Yury Khalyavin <yury.khalyavin@gmail.com>
2016-03-26 00:15:56 +0300

    install.sh install configs

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+ln -s $PWD/_dir_colors ~/.dir_colors
+ln -s $PWD/_gitconfig ~/.gitconfig
+ln -s $PWD/_hgrc ~/.hgrc
+ln -s $PWD/_tmux.conf ~/.tmux.conf
+ln -s $PWD/_vimrc ~/.vimrc
+ln -s $PWD/_zshrc ~/.zshrc
+ln -s $PWD/_zshrc.local ~/.zshrc.local
+ln -s $PWD/_vim ~/.vim
